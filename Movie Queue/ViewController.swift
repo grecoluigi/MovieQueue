f6de6b50fdd2504bb26dde1ccc80e7504a0b6ceb
Luigi Greco <9luigi1@gmail.com>
2020-07-01 16:22:35 +0200

    Initial Commit

---
+++ Movie Queue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Queue
+//
+//  Created by Luigi Greco on 01/07/2020.
+//  Copyright © 2020 Luigi Greco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
